b8575d66d70dfe8421ba5bdd1825e3c2efd448c6
Maxime-Brochet <maxime.brochet.98@gmail.com>
2024-12-11 17:20:37 +0100

    Update route

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,8 +3,8 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
   new Route("/", "Accueil", "/pages/home.html"),
-  new Route("/signin", "Connexion", "/pages/signin.html"),
-  new Route("/signup", "Inscription", "/pages/signup.html"),
+  new Route("/signin", "Connexion", "/pages/auth/signin.html"),
+  new Route("/signup", "Inscription", "/pages/auth/signup.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websiteName
