f565ddae9b114117298dcf38983ff2f6d04e92e6
Maxime-Brochet <maxime.brochet.98@gmail.com>
2024-12-11 17:32:01 +0100

    Add Account page

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,6 +5,7 @@ export const allRoutes = [
   new Route("/", "Accueil", "/pages/home.html"),
   new Route("/signin", "Connexion", "/pages/auth/signin.html"),
   new Route("/signup", "Inscription", "/pages/auth/signup.html"),
+  new Route("/account", "Mon Compte", "/pages/auth/account.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websiteName
